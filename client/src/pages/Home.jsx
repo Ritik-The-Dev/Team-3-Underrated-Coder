5c7c3e5291f9347d5b4e95b9f2fe8ce24107e516
a2istogether <97823872+a2istogether@users.noreply.github.com>
2024-05-23 14:43:24 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,8 +13,6 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const numbers = [1, 2, 3, 4, 5,1, 2, 3, 4, 5];
 
-
-
   const getUserData = async () => {
     setLoading(true);
     try {
